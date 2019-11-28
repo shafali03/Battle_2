6576237c5ac6b47e786f580b36e8e7fa296ce2cd
shafali03 <shaf03sm@gmail.com>
2019-11-28 13:40:19 +0000

    update on players

--- app.rb
+++ app.rb
@@ -1,24 +1,26 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
   
 
-
-
-
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
   
-
-
+  get '/play' do
+    # @message = session[:message]
+    @player_1 = session[:player_1_name]
+    @player_2 = session[:player_2_name]
+    erb :play
+  end
 
   run! if app_file == $0
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,7 +1,6 @@
 
-
-  feature "Enter names" do
-    scenario "Submit name of players" do
+  feature "View points" do
+    scenario "See player 2 points" do
       visit('/')
       fill_in :player_1_name, :with => "Shaf"
       fill_in :player_2_name, :with => "Ellis"
@@ -9,7 +8,3 @@
       expect(page).to have_content "Shaf vs. Ellis"
   end
 end
-
-
-
-
